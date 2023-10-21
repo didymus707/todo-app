31e50a625196f9ce6082dad0144c4fd61ddbfde4
Adewale Orotayo <didymus7007@gmail.com>
2023-10-21 23:32:33 +0100

    added the todo slice and started using typescript in the redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 const rootReducer = combineReducers({});
 export type RootState = ReturnType<typeof rootReducer>;
 
+const store =  configureStore({
+  reducer: {},
+});
+
+export default store
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch; 
